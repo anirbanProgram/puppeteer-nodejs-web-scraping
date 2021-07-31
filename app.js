e9f65784eee4486ba5867375db251f94f0c1dc81
anirbanProgram <50074775+anirbanProgram@users.noreply.github.com>
2021-08-01 03:21:07 +0530

    Update app.js

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.get('/', (req, res) => {
             const page = await browser.newPage()
             //Any url of a flipkart product page
             await page.goto('https://www.flipkart.com/mi-4a-pro-80-cm-32-inch-hd-ready-led-smart-android-tv/p/itmfdwh5jyqhmvzg?pid=TVSG22C4DXQRVSTT&lid=LSTTVSG22C4DXQRVSTTJZD2M0&marketplace=FLIPKART&store=ckf%2Fczl&srno=b_1_1&otracker=browse&fm=factBasedRecommendation%2FrecentlyViewed&iid=2e03c518-b59c-482e-9fbf-8115f7f6931c.TVSG22C4DXQRVSTT.SEARCH&ppt=pp&ppn=pp&ssid=nmlbpx2eds0000001627767974182')
-            await page.waitForTimeout(5000)
+            await page.waitForTimeout(3000)
 
             let productTitle = await page.$$eval('.B_NuCI', title => title.map((title) => { return title.innerText; }));
             console.log("Title: ", productTitle[0])
@@ -41,6 +41,3 @@ app.get('/', (req, res) => {
 const server = app.listen(app.get('port'), () => {
     console.log('Server started at port ' + app.get('port'))
 })
-
-
-module.exports = app
